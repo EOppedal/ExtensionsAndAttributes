cfa0eceaab15fa24bc61c8b91595a676d4bdce8e
EOppedal <119928653+EOppedal@users.noreply.github.com>
2026-03-05 08:39:39 +0100

    added MathUtils

---
+++ Runtime/MathUtils.cs
@@ -0,0 +1,10 @@
+﻿public static class MathUtils {
+    public static int GetGreatestCommonDivisor(int a, int b) {
+        while (a != 0 && b != 0) {
+            if (a > b) a %= b;
+            else b %= a;
+        }
+             
+        return a | b;
+    }
+}
